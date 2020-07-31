09cd60a6a3fa0271d48fb6d9f50c231c3cf02c56
Tomislav Anastasovski <tomislavanastasovski@gmail.com>
2020-07-31 14:53:14 +0200

    Customer Info Initial Page

--- frontend/src/pages/customer/CustomerInfo.js
+++ frontend/src/pages/customer/CustomerInfo.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import axios from "../../axiosConfig/axiosConfig";
+import Badge from "react-bootstrap/Badge";
 
-export default function CustomerInfo()
-{
+export default function CustomerInfo() {
+
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        axios.get(`user/${2}`)
+            .then(res => setUser(res.data))
+            .catch(err => console.log(err))
+    }, [])
+
     return (
         <div>
             User Info
+
+            {user && <div>
+                <p>{user.name} {user.isInstructor && <Badge variant="info">Instructor</Badge> } </p>
+                <p>{user.surname}</p>
+                <p>{user.email}</p>
+                {user.username}
+                <img src={"data:image/jpeg;base64," + user.picture} alt="user"/>
+            </div>}
         </div>)
 }
